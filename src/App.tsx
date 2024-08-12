5e8cb03de5da041f315a119047e8ee0e72b41bb8
charlzyx <charlzyx@qq.com>
2024-08-12 10:12:27 +0800

    feat: 🎸 fix nv dist

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import { getYear } from "date-fns";
-
 import { Divider } from "@geist-ui/core";
-
 import { Hitokoto } from "./components/HiTokoto";
 import SystemIno, { BaseInfo } from "./components/SystemInfo";
 import { BreathOfWild } from "./components/Online";
@@ -9,41 +7,34 @@ import { req } from "./req";
 
 function App() {
   return (
-    <div className="min-h-100vh pt-40px px-4 max-w-5xl mx-auto relative pb-70">
+    <div className="h-100vh pt-20px px-4 max-w-5xl mx-auto relative">
       <header>
         <div className="p-4 max-w-5xl mx-auto">
-          <BaseInfo></BaseInfo>
+          <BaseInfo>
+            <BreathOfWild></BreathOfWild>
+          </BaseInfo>
           <Divider></Divider>
           <div className="text-right text-0.9rem mt-1">
             <Hitokoto />
           </div>
         </div>
       </header>
-      <main className="mt-8 p-4">
+      <main className="p-4">
         <div className="font-300 text-13px flex justify-between items-end w-full">
-          <div className="w-full">
-            <div className="ml-4px">
-              <SystemIno />
-            </div>
-          </div>
+          <SystemIno />
         </div>
       </main>
-      <footer className=" max-w-5xl mx-auto p-3 op-60 mt-8 absolute bottom-2 right-0 left-0">
+      <footer className="op-60 mt-8 absolute bottom-4 right-4 left-0">
         <div className="font-300 text-13px flex justify-between items-end w-full">
           <div className="w-20"></div>
           <div className="text-right dark:op-60">
-            <div
-              onClick={() => {
-                req("/wol");
-              }}
-              className="custom-bg"
-            />
-            <BreathOfWild></BreathOfWild>
+            <div className="custom-bg" />
+
             <div className="mt-2 flex align-center">
               <div className="i-carbon:bat text-4 mr-3 mt-1" />
               <a className="link">Nothing to fear. </a>
             </div>
-            <a  href="https://github.com/charlzyx">
+            <a href="https://github.com/charlzyx">
               <div className="mt-2 flex align-center">
                 <div className="i-carbon:logo-github text-4 mr-3 mt-0" />
                 <span className="op-100">©&nbsp;{getYear(new Date())} </span>
--- src/components/SystemInfo.tsx
+++ src/components/SystemInfo.tsx
@@ -5,179 +5,178 @@ import { Divider } from "@geist-ui/core";
 const units = ["b", "KB", "MB", "GB", "TB"];
 const upper = (x?: string) => (x ? x.replace(/\w/, (m) => m.toUpperCase()) : x);
 const readable = (x: number, toFixed = 2) => {
-	if (Number.isNaN(+x)) return "";
-	let n = x ?? 0;
+  if (Number.isNaN(+x)) return "";
+  let n = x ?? 0;
 
-	let uidx = 0;
-	while (n > 1024) {
-		n = n / 1024;
-		uidx++;
-	}
-	return n.toFixed(toFixed) + units[uidx];
+  let uidx = 0;
+  while (n > 1024) {
+    n = n / 1024;
+    uidx++;
+  }
+  return n.toFixed(toFixed) + units[uidx];
 };
 
 const cent = (a: number, b: number) => {
-	return (((a || 0) / (b || 1)) * 100).toFixed(2);
+  return (((a || 0) / (b || 1)) * 100).toFixed(2);
 };
 
 const Code = (props: { title: string; data: any }) => {
-	return (
-		<>
-			<label>{props.title}</label>
-			<pre>
-				<code>{props.data ? JSON.stringify(props.data, null, 2) : "--"}</code>
-			</pre>
-		</>
-	);
+  return (
+    <>
+      <label>{props.title}</label>
+      <pre>
+        <code>{props.data ? JSON.stringify(props.data, null, 2) : "--"}</code>
+      </pre>
+    </>
+  );
 };
 
-export const BaseInfo = () => {
-	const { data: os } = si.useOsInfo();
-	const { data: system } = si.useSystem();
-	const { data: networkStatus } = si.useNetworkStats();
-	// const { data: ifaces } = si.useNetworkInterfaces();
-	useEffect(() => {
-		window.document.title = `Far.${system?.manufacturer}® ${system?.model} ${system?.version}`;
-	}, [system]);
+export const BaseInfo = (props: { children: React.ReactNode }) => {
+  const { data: os } = si.useOsInfo();
+  const { data: system } = si.useSystem();
+  const { data: networkStatus } = si.useNetworkStats();
+  // const { data: ifaces } = si.useNetworkInterfaces();
+  useEffect(() => {
+    window.document.title = `Far.${system?.manufacturer}® ${system?.model} ${system?.version}`;
+  }, [system]);
 
-	return (
-		<div className="flex justify-between text-3">
-			<div>
-				<div className="flex text-5 mb-3 mt-3 lh-8">
-					<div className="i-carbon:airplay text-6 mr-2" />
-					{system?.manufacturer}&reg; {system?.model} {system?.version}
-				</div>
-				<div className="text-3">
-					{upper(os?.distro)} @{os?.hostname} {os?.arch}.{os?.platform}
-				</div>
-				<div className="flex mt-2">
-					<div className="i-carbon:server-proxy text-4 mr-3" />
-					<div>
-						{networkStatus?.map((net) => {
-							return (
-								<div>
-									<div>
-										Rx {readable(net.rx_sec!)}/s Tx {readable(net.tx_sec!)}/s
-									</div>
-								</div>
-							);
-						})}
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="flex justify-between text-3">
+      <div>
+        <div className="flex text-5 mb-2 mt-3 lh-8">
+          <div className="i-carbon:airplay text-6 mr-2" />
+          <div className="flex mr-2 mt-2">{props.children}</div>
+          {system?.manufacturer}&reg; {system?.model} {system?.version}
+        </div>
+        <div className="text-3">
+          {upper(os?.distro)} @{os?.hostname} {os?.arch}.{os?.platform}
+        </div>
+        <div className="flex mt-2">
+          <div className="i-carbon:server-proxy text-4 mr-3" />
+          <div>
+            {networkStatus?.map((net) => {
+              return (
+                <div>
+                  <div>
+                    Rx {readable(net.rx_sec!)}/s Tx {readable(net.tx_sec!)}/s
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default function SystemIno() {
-	const { data: mem } = si.useMem();
-	const { data: memLayout } = si.useMemLayout();
-	const { data: disk } = si.useDiskLayout();
-	const { data: fsSize } = si.useFsSize();
-	// const { data: baseBoard } = si.useBaseboard();
-	const { data: graphics } = si.useGraphics();
-	const { data: cpu } = si.useCpu();
+  const { data: mem } = si.useMem();
+  const { data: memLayout } = si.useMemLayout();
+  const { data: disk } = si.useDiskLayout();
+  const { data: fsSize } = si.useFsSize();
+  // const { data: baseBoard } = si.useBaseboard();
+  const { data: graphics } = si.useGraphics();
+  const { data: cpu } = si.useCpu();
 
-	const graphic = useMemo(() => {
-		return graphics?.controllers?.[0] ?? null;
-	}, [graphics]);
+  const graphic = useMemo(() => {
+    return graphics?.controllers?.[0] ?? null;
+  }, [graphics]);
 
-	return (
-		<div className="children:mb-4 text-3.2 lh-4.4">
-			<div className="flex items-top sm:flex-row flex-col children:mb-2">
+  return (
+    <div className="children:mb-4 text-3.2 lh-4.4">
+      <div className="flex items-top sm:flex-row flex-col children:mb-2">
         <div className="i-carbon:chip text-4 mr-3 mt-0.5" />
-				<div className="flex text-3.8 mr-4 w-12 keepword">
-					处理器
-				</div>
-				<div>
-					<div className="text-4 mb-2">
-						{cpu?.manufacturer}&reg; {cpu?.brand}
-					</div>
-					<div>
-						{cpu?.physicalCores} 核 @{cpu?.speed}GHz {cpu?.cores} 线程
-					</div>
-				</div>
-			</div>
-			<div className="flex items-top sm:flex-row flex-col children:mb-2">
-				{/* biome-ignore lint/a11y/noSvgWithoutTitle: <explanation> */}
-				<svg
-					className="mr-3"
-					stroke="currentColor"
-					fill="currentColor"
-					strokeWidth="0"
-					viewBox="0 0 640 512"
-					height="1rem"
-					width="1rem"
-					xmlns="http://www.w3.org/2000/svg"
-				>
-					<path d="M640 130.94V96c0-17.67-14.33-32-32-32H32C14.33 64 0 78.33 0 96v34.94c18.6 6.61 32 24.19 32 45.06s-13.4 38.45-32 45.06V320h640v-98.94c-18.6-6.61-32-24.19-32-45.06s13.4-38.45 32-45.06zM224 256h-64V128h64v128zm128 0h-64V128h64v128zm128 0h-64V128h64v128zM0 448h64v-26.67c0-8.84 7.16-16 16-16s16 7.16 16 16V448h128v-26.67c0-8.84 7.16-16 16-16s16 7.16 16 16V448h128v-26.67c0-8.84 7.16-16 16-16s16 7.16 16 16V448h128v-26.67c0-8.84 7.16-16 16-16s16 7.16 16 16V448h64v-96H0v96z" />
-				</svg>
-				{/* <div className="i-carbon:edge-node text-4 mr-3 mt-0.5" /> */}
-				<div className="text-3.8 mr-4 w-12 keepword">
-					内<span className="op-0">条</span>存
-				</div>
-				<div>
-					<div className="text-4 mb-2">
-						{mem ? cent(mem.free, mem.total) : 0}% Free &nbsp;
-						{readable(mem?.total!)}
-					</div>
-					{memLayout?.map((ml) => {
-						return ml.manufacturer ? (
-							<div>
-								<div>
-									{ml?.manufacturer} {ml?.type} {readable(ml.size)}
-								</div>
-							</div>
-						) : null;
-					})}
-				</div>
-			</div>
+        <div className="flex text-3.8 mr-4 w-12 keepword">处理器</div>
+        <div>
+          <div className="text-4 mb-2">
+            {cpu?.manufacturer}&reg; {cpu?.brand}
+          </div>
+          <div>
+            {cpu?.physicalCores} 核 @{cpu?.speed}GHz {cpu?.cores} 线程
+          </div>
+        </div>
+      </div>
+      <div className="flex items-top sm:flex-row flex-col children:mb-2">
+        {/* biome-ignore lint/a11y/noSvgWithoutTitle: <explanation> */}
+        <svg
+          className="mr-3"
+          stroke="currentColor"
+          fill="currentColor"
+          strokeWidth="0"
+          viewBox="0 0 640 512"
+          height="1rem"
+          width="1rem"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path d="M640 130.94V96c0-17.67-14.33-32-32-32H32C14.33 64 0 78.33 0 96v34.94c18.6 6.61 32 24.19 32 45.06s-13.4 38.45-32 45.06V320h640v-98.94c-18.6-6.61-32-24.19-32-45.06s13.4-38.45 32-45.06zM224 256h-64V128h64v128zm128 0h-64V128h64v128zm128 0h-64V128h64v128zM0 448h64v-26.67c0-8.84 7.16-16 16-16s16 7.16 16 16V448h128v-26.67c0-8.84 7.16-16 16-16s16 7.16 16 16V448h128v-26.67c0-8.84 7.16-16 16-16s16 7.16 16 16V448h128v-26.67c0-8.84 7.16-16 16-16s16 7.16 16 16V448h64v-96H0v96z" />
+        </svg>
+        {/* <div className="i-carbon:edge-node text-4 mr-3 mt-0.5" /> */}
+        <div className="text-3.8 mr-4 w-12 keepword">
+          内<span className="op-0">条</span>存
+        </div>
+        <div>
+          <div className="text-4 mb-2">
+            {mem ? cent(mem.free, mem.total) : 0}% Free &nbsp;
+            {readable(mem?.total!)}
+          </div>
+          {memLayout?.map((ml) => {
+            return ml.manufacturer ? (
+              <div>
+                <div>
+                  {ml?.manufacturer} {ml?.type} {readable(ml.size)}
+                </div>
+              </div>
+            ) : null;
+          })}
+        </div>
+      </div>
 
-			<div className="flex items-top sm:flex-row flex-col children:mb-2">
-				<div className="i-carbon:archive text-4 mr-3 mt-0.5" />
-				<div className="text-3.8 mr-4 w-12 keepword">
-					硬<span className="op-0">软</span>盘
-				</div>
-				<div>
-					{disk?.map((dd) => {
-						return (
-							<div className="text-4 mb-2 ">
-								{dd?.name} {dd?.type} {readable(dd.size)}
-							</div>
-						);
-					})}
-					{fsSize?.map((pp) => {
-						return /cloud/.test(pp.mount) ? (
-							<div className="flex flex-gap-8">
-								<div>{cent(pp.available, pp.size)}%</div>
-								<div>{readable(pp.available)}</div>
-								<div>{readable(pp.size)}</div>
-								<div>{pp.mount}</div>
-							</div>
-						) : null;
-					})}
-				</div>
-			</div>
-			{graphic ? (
-				<div className="flex items-top sm:flex-col sm:children:mb-2sm:flex-row">
-					<div className="i-carbon:4k text-4 mr-3 mt-0.5" />
-					<div className="text-3.8 mr-4 w-12 keepword">
-						显<span className="op-0">卡</span>卡
-					</div>
-					<div>
-						<div className="text-4 mb-2">
-							{graphic?.vendor}&reg; {graphic?.model}
-						</div>
-						<div>
-							<div>
-								{graphic?.cores}核 {readable(graphic?.memoryFree!)}/
-								{readable(graphic?.memoryTotal!)}
-							</div>
-						</div>
-					</div>
-				</div>
-			) : null}
-			<Divider></Divider>
-		</div>
-	);
+      <div className="flex items-top sm:flex-row flex-col children:mb-2">
+        <div className="i-carbon:archive text-4 mr-3 mt-0.5" />
+        <div className="text-3.8 mr-4 w-12 keepword">
+          硬<span className="op-0">软</span>盘
+        </div>
+        <div>
+          {disk?.map((dd) => {
+            return (
+              <div className="text-4 mb-2 ">
+                {dd?.name} {dd?.type} {readable(dd.size)}
+              </div>
+            );
+          })}
+          {fsSize?.map((pp) => {
+            return /cloud/.test(pp.mount) ? (
+              <div className="flex flex-gap-8">
+                <div>{cent(pp.available, pp.size)}%</div>
+                <div>{readable(pp.available)}</div>
+                <div>{readable(pp.size)}</div>
+                <div>{pp.mount}</div>
+              </div>
+            ) : null;
+          })}
+        </div>
+      </div>
+      {graphic ? (
+        <div className="flex items-top sm:flex-row flex-col children:mb-2">
+          <div className="i-carbon:4k text-4 mr-3 mt-0.5" />
+          <div className="text-3.8 mr-4 w-12 keepword">
+            显<span className="op-0">卡</span>卡
+          </div>
+          <div>
+            <div className="text-4 mb-2">
+              {graphic?.vendor}&reg; {graphic?.model}
+            </div>
+            <div>
+              <div>
+                {graphic?.cores}核 {readable(graphic?.memoryFree!)}/
+                {readable(graphic?.memoryTotal!)}
+              </div>
+            </div>
+          </div>
+        </div>
+      ) : null}
+      <Divider></Divider>
+    </div>
+  );
 }
