4abc9ba7a47de337991ee1a66204d45a77b64c4f
charlzyx <charlzyx@qq.com>
2024-08-12 11:27:17 +0800

    feat: 🎸 fix cli called

--- server/app.ts
+++ server/app.ts
@@ -37,14 +37,12 @@ const routes: Record<
     return offline ? "offline" : "online";
   },
   "/update": async (req) => {
-    const cli = `
+    await $`cd /opt/nv
     wget https://github.com/charlzyx/nv/releases/download/master/nv.tar.gz -O nv.tar.gz
     rm -rf dist server package.json
     tar -xvf nv.tar.gz 
     bun i --production
-    service nv restart
-    `;
-    await $`${cli}`;
+    service nv restart`;
   },
   "/si": async (req) => {
     const url = new URL(req.url);
